6f5c8f301a3278cc5df037e348185d3688f64d20
wali.rahman89@yahoo.com <wali.rahman89@yahoo.com>
2017-11-12 15:08:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ console.log('this code was called');
 
 console.log(driver + 'is the driver variable name');
 
-const info = 'is the driver variable name';
-console.log(driver + info);
-
+//const info = 'is the driver variable name';
+//console.log(driver + info);
