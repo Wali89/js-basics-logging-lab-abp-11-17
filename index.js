e32c4153243eeb617db76a42c6ff6db39ac166f4
wali.rahman89@yahoo.com <wali.rahman89@yahoo.com>
2017-11-12 15:00:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const driver = 'bob';
 
-console.log ('this code was called');
+console.log('this code was called');
 
-console.log (driver);
+console.log(driver);
 
 console.log("bob" + "is the driver variable name");
